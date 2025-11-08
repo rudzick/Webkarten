e0c8b1152ffe6b2ea6fdcc5cebc11940b9cbb03d
Oliver Rudzick <oliver@rudzick.de>
2025-11-08 18:35:42 +0100

    maplibre-client for shortbread-obstbaumkarte

---
+++ obstbaumkarte_ml/src/main.ts
@@ -0,0 +1,19 @@
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.ts'
+import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
+
+const map = new maplibregl.Map({
+    container: 'map', // container id
+    style: 'map_style.json', // style URL
+    center: [13.31233, 52.47416], // starting position [lng, lat]
+    zoom: 17 // starting zoom
+});
+
+	map.on('load', async () => {
+//        	       image = await map.loadImage('https://commons.wikimedia.org/wiki/File:WLE_garden.svg');
+        	       const image = await map.loadImage('WLE_garden.png');
+        map.addImage('obstbaum', image.data);
+    });
