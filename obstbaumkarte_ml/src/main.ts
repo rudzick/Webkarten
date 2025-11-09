b61f2cba02e9d5509094b0b0c9935b3bc301eae6
Oliver Rudzick <oliver@rudzick.de>
2025-11-09 12:53:44 +0100

    added versatiles colorfull style

--- obstbaumkarte_ml/src/main.ts
+++ obstbaumkarte_ml/src/main.ts
@@ -4,7 +4,8 @@ import 'maplibre-gl/dist/maplibre-gl.css';
 
 const map = new maplibregl.Map({
     container: 'map', // container id
-    style: 'map_style.json', // style URL
+//    style: 'map_style.json', // style URL
+    style: 'myversatiles_colorfull_style.json', // style URL
     center: [13.31233, 52.47416], // starting position [lng, lat]
     zoom: 17 // starting zoom
 });
@@ -14,3 +15,10 @@ const map = new maplibregl.Map({
         	       const image = await map.loadImage('WLE_garden.png');
         map.addImage('obstbaum', image.data);
     });
+    // Add zoom and rotation controls to the map.
+    map.addControl(new maplibregl.NavigationControl({
+        visualizePitch: true,
+        visualizeRoll: true,
+        showZoom: true,
+        showCompass: true
+    }));
