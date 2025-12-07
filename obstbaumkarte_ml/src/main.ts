a9c60677f1a25a4d1a2ebbc37072652c90219d91
rudzick <oliver@rudzick.de>
2025-12-07 13:51:39 +0100

    Funtion zur Anzeige von Features bei hover eingebaut. Compiliert noch nicht mit npm run build

--- obstbaumkarte_ml/src/main.ts
+++ obstbaumkarte_ml/src/main.ts
@@ -26,4 +26,43 @@ const map = new maplibregl.Map({
     //	maxWidth: 100,
     	unit: 'metric'
     }));
-    
+
+// Show tags of OSM objects in the lower left corner on hover:
+
+    map.on('mousemove', (e) => {const features = map.queryRenderedFeatures(
+			e.point,
+		{ layers: [ 'public.trees','public.trees_17'] }
+	);		
+
+        // Limit the number of properties we're displaying for
+        // legibility and performance
+        const displayProperties = [
+         //   'type',
+            'properties',
+         //   'id',
+         //   'layer',
+         //   'source',
+         //   'sourceLayer',
+         //   'state'
+        ];
+	
+        const displayFeatures = features.map((feat) => {
+            const displayFeat:  {[key: string]:any} = {};
+            displayProperties.forEach((prop) => {
+                displayFeat[prop] = feat[prop];
+            });
+            return displayFeat;
+        });
+
+  	 if (displayFeatures.length > 0){
+   		document.getElementById('features').style.background = 'rgba(255, 255, 255, 0.8)';
+            document.getElementById('features').innerHTML = JSON.stringify(
+            	displayFeatures,
+		 null,
+           	 2
+        );
+	} else {
+		document.getElementById('features').innerHTML = ''
+    		document.getElementById('features').style.background = 'rgba(255, 255, 255, 0.0)';
+	      }
+    });
