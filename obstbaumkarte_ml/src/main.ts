108bd305cc8044b53fa40afbd3b417810780e093
rudzick <oliver@rudzick.de>
2025-12-14 01:16:23 +0100

    changed javascript to typescript

--- obstbaumkarte_ml/src/main.ts
+++ obstbaumkarte_ml/src/main.ts
@@ -29,7 +29,7 @@ const map = new maplibregl.Map({
 
 // Show tags of OSM objects in the lower left corner on hover:
 
-    map.on('mousemove', (e) => {const features = map.queryRenderedFeatures(
+    map.on('mousemove', (e : any) => {const features = map.queryRenderedFeatures(
 			e.point,
 		{ layers: [ 'public.trees','public.trees_17'] }
 	);		
@@ -46,7 +46,7 @@ const map = new maplibregl.Map({
          //   'state'
         ];
 	
-        const displayFeatures = features.map((feat) => {
+        const displayFeatures = features.map((feat : any) => {
             const displayFeat:  {[key: string]:any} = {};
             displayProperties.forEach((prop) => {
                 displayFeat[prop] = feat[prop];
@@ -55,14 +55,14 @@ const map = new maplibregl.Map({
         });
 
   	 if (displayFeatures.length > 0){
-   		document.getElementById('features').style.background = 'rgba(255, 255, 255, 0.8)';
-            document.getElementById('features').innerHTML = JSON.stringify(
+   		document.getElementById('features')!.style.background = 'rgba(255, 255, 255, 0.8)';
+            document.getElementById('features')!.innerHTML = JSON.stringify(
             	displayFeatures,
 		 null,
            	 2
         );
 	} else {
-		document.getElementById('features').innerHTML = ''
-    		document.getElementById('features').style.background = 'rgba(255, 255, 255, 0.0)';
+		document.getElementById('features')!.innerHTML = ''
+    		document.getElementById('features')!.style.background = 'rgba(255, 255, 255, 0.0)';
 	      }
     });
